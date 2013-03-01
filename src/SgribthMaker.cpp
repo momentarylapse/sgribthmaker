383839471b68174c9ff9196963edf8ec474faa39
Michael Ankele <michi@lupina.de>
2013-03-01 21:25:16 +0100

    lib update

--- src/SgribthMaker.cpp
+++ src/SgribthMaker.cpp
@@ -15,7 +15,7 @@ string Filename = "";
 
 
 string AppTitle = "SgribthMaker";
-string AppVersion = "0.3.19.1";
+string AppVersion = "0.3.19.2";
 
 #define ALLOW_LOGGING			true
 //#define ALLOW_LOGGING			false
