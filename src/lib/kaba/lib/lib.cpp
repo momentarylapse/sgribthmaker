4787f8bb1e0852b7c9aae341c4eda6f7f1f392f9
Michael Ankele <michi@lupina.de>
2019-12-09 17:03:16 +0100

    lib update

--- src/lib/kaba/lib/lib.cpp
+++ src/lib/kaba/lib/lib.cpp
@@ -30,7 +30,7 @@
 
 namespace Kaba{
 
-string LibVersion = "0.17.10.2";
+string LibVersion = "0.17.10.3";
 
 
 const string IDENTIFIER_CLASS = "class";
@@ -466,7 +466,7 @@ void class_add_const(const string &name, const Class *type, const void *value) {
 	// enums can't be referenced...
 	if (type == TypeInt)
 		*(const void**)c->p() = value;
-	else
+	else if (value)
 		memcpy(c->p(), value, type->size);
 }
 
