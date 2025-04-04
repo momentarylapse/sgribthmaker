25d1b3a50692d4277fd8b7610147a9d9d45cbca7
Michael Ankele <michi@lupina.de>
2025-04-04 23:30:57 +0200

    [lib] update

--- src/lib/math/Box.h
+++ src/lib/math/Box.h
@@ -10,5 +10,9 @@ struct Box {
 	Box canonical() const;
 	vec3 to_relative(const vec3& p) const;
 	vec3 to_absolute(const vec3& p) const;
+	string str() const;
+
+	Box operator||(const Box& b) const;
+	Box operator&&(const Box& b) const;
 };
 
